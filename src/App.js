56dab11f48ff805e32bc1884c83d695ed3ad1db2
Keshav-mittal <aggarwalkeshav001@gmail.com>
2023-06-24 21:28:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+let id_number = 1;
 
-function App() {
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [ArrayOfData, setArrayOfData] = useState([]);
+
+  const AddTodo = () => {
+    setArrayOfData([
+      ...ArrayOfData,
+      {
+        id: id_number++,
+        todo: input
+      }]
+    );
+    setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='container d-flex flex-column'>
+      <div>
+        <h1 className='m-5 justify-content-center'>𝕴𝖔𝖉𝖔 𝕷𝖎𝖘𝖙</h1>
 
-export default App;
+        <div class="input-group mb-6">
+          <input type="text" className="form-control" placeholder="Add your todo task" aria-label="Add your Todo Task" aria-describedby="basic-addon2" value={input} onChange={e => setInput(e.target.value)} />
+          <div class="input-group-append">
+            <button class="btn btn-dark" onClick={AddTodo} >ADD</button>
+          </div>
+        </div>
+        <ul >
+          {ArrayOfData.map(data => (
+            <li key={data?.id} className='card' style={{ width: "auto", height: "70px", margin: "2rem" }}>
+              <div className='card-body d-flex justify-content-between'>
+                <label className=' text-blue  mt-auto mb-auto '>{data?.todo?.length > 0 ? data?.todo : data.todo = prompt("Enter some data")}</label>
+                <button className="delete btn btn-dark" onClick={() => setArrayOfData(ArrayOfData.filter((em) => em?.id !== data?.id))}>
+                  Delete
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul >
+
+      </div >
+    </div >
+  )
+};
+
+export default App
